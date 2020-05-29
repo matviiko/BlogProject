0cb72854ec63f75130aeaf22357dbf3c0ba348c4
matviiko <matviy.kopchak@gmail.com>
2020-05-29 22:03:07 +0300

    create create page-post html and css

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbRatingConfig} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
+  providers: [NgbRatingConfig]
 
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbRatingConfig) {
+    config.max = 5;
+    config.readonly = true;
+  }
 
   ngOnInit(): void {
   }
