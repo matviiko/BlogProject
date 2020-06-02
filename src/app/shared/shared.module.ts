3008854c32c276fa500c18a618f68fd37015b040
matviiko <matviy.kopchak@gmail.com>
2020-06-02 16:20:04 +0300

    Add Quill

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import {NgModule} from "@angular/core";
 import {HttpClientModule} from "@angular/common/http";
 import {NgbAlertModule, NgbModule, NgbPaginationModule} from "@ng-bootstrap/ng-bootstrap";
+import {QuillModule} from 'ngx-quill';
 
 @NgModule({
   imports: [
     HttpClientModule,
     NgbPaginationModule,
     NgbAlertModule,
-    NgbModule
+    NgbModule,
+    QuillModule.forRoot()
   ],
   exports: [
-    HttpClientModule
+    HttpClientModule,
+    QuillModule
   ]
 })
 export class SharedModule {
