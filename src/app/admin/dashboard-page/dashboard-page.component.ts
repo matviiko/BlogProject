b37fb14bc9580afd32001d0156e4458a28b17d40
matviiko <matviy.kopchak@gmail.com>
2020-06-03 17:33:11 +0300

    Create Dashboard-page and getAll() in PostService

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from '../../shared/post.service';
+import {Post} from '../../shared/interfaces';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  postsSub: Subscription;
 
-  ngOnInit(): void {
+  constructor(private postsService: PostsService) {
   }
 
+  ngOnInit() {
+    this.postsSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts;
+    });
+
+  }
+
+  ngOnDestroy() {
+    if (this.postsSub) {
+      this.postsSub.unsubscribe();
+    }
+  }
+
+  remove(id: string) {
+
+  }
 }
