8a3148d7635df8fc96d67b30036a350f95ce9f29
matviiko <matviy.kopchak@gmail.com>
2020-06-24 23:23:10 +0300

    use input in sidebar-component

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CategoriesService } from '../../services/categories.service';
 import { Observable } from 'rxjs';
 import { Category, Post } from '../../interfaces';
-import { PostsService } from '../../post.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,18 +10,12 @@ import { PostsService } from '../../post.service';
 })
 export class SidebarComponent implements OnInit {
   categories$: Observable<Category[]>;
-  posts$: Observable<Post[]>;
-  posts: Array<Post>;
 
-  constructor(private categoriesService: CategoriesService, private postsService: PostsService) {}
+  @Input('sidebarPosts') posts: Array<Post>;
+
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit() {
     this.categories$ = this.categoriesService.getAllCategories();
-
-    this.posts$ = this.postsService.getAll();
-
-    this.posts$.subscribe(posts => {
-      this.posts = posts;
-    });
   }
 }
