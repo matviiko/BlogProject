13e0f0f89ca00aaaec5c4e2d47c447be9055fe67
matviiko <matviy.kopchak@gmail.com>
2020-07-01 22:22:18 +0300

    add user service

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ProfileUser } from '../interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<ProfileUser[]> {
+    return this.http.get(`${environment.fbDbUrl}/users.json`).pipe(
+      map((response: { [key: string]: any }) => {
+        return Object.keys(response).map(key => ({
+          ...response[key],
+        }));
+      })
+    );
+  }
+}
