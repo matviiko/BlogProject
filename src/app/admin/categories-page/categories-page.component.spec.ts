b094c26ef7f7ca587363b28410c42850e2e8ff5c
matviiko <matviy.kopchak@gmail.com>
2020-08-03 00:14:42 +0300

    unit test for categories page

--- src/app/admin/categories-page/categories-page.component.spec.ts
+++ src/app/admin/categories-page/categories-page.component.spec.ts
@@ -1,7 +1,7 @@
 import { CategoriesPageComponent } from './categories-page.component';
 import { CategoriesService } from '../../shared/services/categories.service';
 import { AlertService } from '../shared/services/alert.service';
-import { EMPTY, of } from 'rxjs';
+import { EMPTY, Observable, of } from 'rxjs';
 
 describe('CategoriesPageComponent', () => {
   let component: CategoriesPageComponent;
@@ -66,5 +66,59 @@ describe('CategoriesPageComponent', () => {
     component.removeCategory('10');
 
     expect(spy).toHaveBeenCalledWith('10');
+  });
+
+  it('should return data from backEnd', function () {
+    const spy = spyOn(service, 'getAllCategories').and.returnValue(
+      of([
+        {
+          name: 'Category',
+          id: 'id',
+          date: new Date(),
+        },
+        {
+          name: 'Category2',
+          id: 'id2',
+          date: new Date(),
+        },
+      ])
+    );
+
+    component.ngOnInit();
+    expect(component.categories.length).toBe(2);
+  });
+
+  it('should return null if form invalid', function () {
+    const spy = spyOn(service, 'getAllCategories').and.returnValue(EMPTY);
+    component.ngOnInit();
+    let control = component.form.get('name');
+    control.setValue('');
+    component.submit();
+
+    expect(component.form.invalid).toBeTruthy();
+  });
+
+  it('should unsubscribe CategoriesSub in ngOnDestroy', function () {
+    component.categoriesSub = EMPTY.subscribe();
+    const spy = spyOn(component.categoriesSub, 'unsubscribe');
+    component.ngOnDestroy();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should unsubscribe deleteSub in  ngOnDestroy', function () {
+    component.deleteSub = EMPTY.subscribe();
+    const spy = spyOn(component.deleteSub, 'unsubscribe');
+    component.ngOnDestroy();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should unsubscribe editSub in  ngOnDestroy', function () {
+    component.editSub = EMPTY.subscribe();
+    const spy = spyOn(component.editSub, 'unsubscribe');
+    component.ngOnDestroy();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
