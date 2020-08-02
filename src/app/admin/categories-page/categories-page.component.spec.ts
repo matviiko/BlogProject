f74dc7507e861fe9be1c026f46713813ddb1b07e
matviiko <matviy.kopchak@gmail.com>
2020-08-02 12:13:04 +0300

    Add unit test for categories page

---
+++ src/app/admin/categories-page/categories-page.component.spec.ts
@@ -0,0 +1,70 @@
+import { CategoriesPageComponent } from './categories-page.component';
+import { CategoriesService } from '../../shared/services/categories.service';
+import { AlertService } from '../shared/services/alert.service';
+import { EMPTY, of } from 'rxjs';
+
+describe('CategoriesPageComponent', () => {
+  let component: CategoriesPageComponent;
+  let service: CategoriesService;
+  let alertService: AlertService;
+
+  beforeEach(() => {
+    service = new CategoriesService(null);
+    component = new CategoriesPageComponent(service, alertService);
+    alertService = new AlertService();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('form have control', function () {
+    const spy = spyOn(service, 'getAllCategories').and.callFake(() => {
+      return EMPTY;
+    });
+
+    component.ngOnInit();
+
+    expect(component.form.contains('name')).toBeTruthy();
+  });
+
+  it('should mark name as invalid if empty value', function () {
+    const spy = spyOn(service, 'getAllCategories').and.callFake(() => {
+      return EMPTY;
+    });
+    component.ngOnInit();
+    let control = component.form.get('name');
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
+  });
+
+  it('should add new category', function () {
+    const spyGetAll = spyOn(service, 'getAllCategories').and.callFake(() => {
+      return EMPTY;
+    });
+    const spyCreate = spyOn(service, 'create').and.callFake(() => {
+      return of({
+        name: 'Category',
+        id: 'id',
+        date: new Date(),
+      });
+    });
+    component.ngOnInit();
+    let control = component.form.get('name');
+    control.setValue('Category');
+    component.submit();
+
+    expect(spyCreate).toHaveBeenCalled();
+    expect(component.categories.length).toBe(1);
+  });
+
+  it('should delete category when use removeCategory and user confirm', function () {
+    const spy = spyOn(service, 'remove').and.returnValue(EMPTY);
+    spyOn(window, 'confirm').and.returnValue(true);
+
+    component.removeCategory('10');
+
+    expect(spy).toHaveBeenCalledWith('10');
+  });
+});
